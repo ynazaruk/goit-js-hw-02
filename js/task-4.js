3c1df7778682c373fff96c19d52db36117b34d28
ynazaruk <nazaruk.ya@gmail.com>
2024-01-26 00:24:03 +0200

    hw-02

---
+++ js/task-4.js
@@ -0,0 +1,36 @@
+function getShippingCost(country) {
+  const priceAustralia = "170";
+  const priceChina = "100";
+  const priceChile = "250";
+  const priceJamaica = "120";
+
+  switch (country) {
+    case "Australia":
+      return `Shipping to ${country} will cost ${priceAustralia} credits`;
+      break;
+    case "Germany":
+      return "Sorry, there is no delivery to your country";
+      break;
+    case "China":
+      return `Shipping to ${country} will cost ${priceChina} credits`;
+      break;
+    case "Chile":
+      return `Shipping to ${country} will cost ${priceChile} credits`;
+      break;
+    case "Jamaica":
+      return `Shipping to ${country} will cost ${priceJamaica} credits`;
+      break;
+    case "Sweden":
+      return "Sorry, there is no delivery to your country";
+      break;
+    default:
+      break;
+  }
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
